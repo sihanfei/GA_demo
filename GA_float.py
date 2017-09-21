6b6fe0e60c98a76cdfda74f6c52e23d0be25491a
sihanfei <taosheng@catsti.com>
2017-09-21 20:38:32 +0800

    v0.1

---
+++ GA_float.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 21 18:40:18 2017
+
+@author: taosheng
+
+浮点数方法进行遗传算法
+"""
+
+import numpy as np
+
+"""############################################################################
+purpse：轮盘赌方法随机选择1个DNA
+input 
+  Pf: 各DNA生存率（按照升序排列）
+  prei: 不能被选择的数
+output：
+  i：被选中的DNA的序号
+"""
+def RatioSelect(Pf, prei):
+  i=prei
+  while i==prei:
+    m=0
+    r=np.random.random()
+    for i in range(np.size(Pf)):
+      m=m+Pf[i]
+      if r<m:
+        break
+  return i
+
+"""############################################################################
+purpse：进行两个DNA的粒交换
+input： 
+  M1，M2：输入DNA
+  alpha：交叉比例
+output：
+  Cm1，Cm2：交换后的DNA
+"""
+def CrossOver(M1, M2, alpha):
+  Cm1 = M1*alpha + (1-alpha)*M2
+  Cm2 = M2*alpha + (1-alpha)*M1
+  return Cm1, Cm2
+
+"""############################################################################
+purpse：DNA的变异
+input： 
+  M：输入DNA
+  ratiok：变异比例步长
+output：
+  M：变换后的DNA
+"""
+def Mutation(M, Mmin, Mmax, ratiok):
+  r = np.random.randint(10)
+  if r%2==0:
+    M = M+ratiok*(Mmax-M)*r/10
+  else:
+    M = M-ratiok*(M-Mmin)*r/10
+  return M
+
+"""############################################################################
+遗传算法实际：f(x)=x*sin(10*pi*x)+2, x~[-1,2],精度0.01
+Bn：（2-（-1））／0.01=300<2^9
+"""
+Mmax = 2.0
+Mmin = -1.0
+Mn = 20 # 种群个体数
+T = 500 # 代际数
+Pc = 0.8
+Pm = 0.1
+alpha = 0.3
+ratiok = 0.1
+
+# 初始化
+G = np.random.random(Mn)*2-1 # 生成一个初始族群
+func = G*np.sin(10*np.pi*G)+2 # 计算生存率：值
+pf = func/sum(func) # 归一化生存率
+pfsort = np.argsort(pf) # 生存率排序的序号
+pf = np.sort(pf)
+
+for tnum in range(T):
+  Gs = np.zeros(Mn) # 空子代
+  for inum in range(Mn/2):
+    n0 = RatioSelect(pf, -1) # 选择DNA
+    n1 = RatioSelect(pf, n0) # 选择DNA
+    r = np.random.random()
+    if r<Pc: # 交叉
+      Gs[inum*2],Gs[inum*2+1]=CrossOver(G[pfsort[n0]], G[pfsort[n1]], alpha)
+    else:
+      Gs[inum*2]=G[pfsort[n0]]
+      Gs[inum*2+1]=G[pfsort[n1]]
+  
+    r = np.random.random()
+    if r<Pm: #变异
+      Gs[inum*2] = Mutation(Gs[inum*2], Mmin, Mmax, ratiok)
+
+    r = np.random.random()
+    if r<Pm: #变异
+      Gs[inum*2+1] = Mutation(Gs[inum*2+1], Mmin, Mmax, ratiok)
+
+  G = Gs    
+  func = G*np.sin(10*np.pi*G)+2 # 计算生存率：值
+  pf = func/sum(func) # 归一化生存率
+  pfsort = np.argsort(pf) # 生存率排序的序号
+  pf = np.sort(pf)
+          
+print 'maxfunc=%f' %(np.max(func))
+  
